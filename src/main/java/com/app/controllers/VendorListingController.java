529d2777347311c06b37ca164919e0ea7864e6f3
Swamy Kunta <swamy.kunta@gmail.com>
2024-12-13 23:25:47 -0500

    Vendor profile changes

--- src/main/java/com/app/controllers/VendorListingController.java
+++ src/main/java/com/app/controllers/VendorListingController.java
@@ -1,6 +1,5 @@
 package com.app.controllers;
 
-import org.springframework.data.repository.query.Param;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
